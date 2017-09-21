423806294a4abc4cd6ad9aaa9ae32ade2b08502a
hanxueh <hanxueh@sfu.ca>
2017-09-21 08:31:16 -0700

    google map update

--- js/contact-main.js
+++ js/contact-main.js
@@ -2,9 +2,8 @@ $(document).ready(function(){
 	// google map
 		var map;
 		function initMap() {
-			var uluru = {lat: 48.466, lng: -123.304};
 		  map = new google.maps.Map(document.getElementById('map'), {
-		    center: uluru,
+		    center: {lat: 48.466, lng: -123.304},
 		    zoom: 8
 		  });
 		}
